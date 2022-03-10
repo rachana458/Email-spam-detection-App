9f73b0fafc3d7e1488b6de83c9e1bc1207e83cc2
Rachana Yadav <85185125+rachana458@users.noreply.github.com>
2022-03-10 21:46:02 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+#importing the modules
+from flask import Flask,render_template,request,jsonify
+import pandas as pd
+import numpy as np 
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load('spam_model.pkl')
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+  if request.method == 'POST':
+    message = request.form.get('message')
+    output = model.predict([message])
+    if output == [0]:
+      result = "This Message is Not a SPAM Message."
+    else:
+      result = "This Message is a SPAM Message." 
+    return render_template('index1.html', result=result,message=message)      
+
+  else:
+    return render_template('index1.html')  
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
